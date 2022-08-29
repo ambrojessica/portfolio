89451eec1e863fee5a3d57f10273d17d72759755
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-29 17:31:44 -0600

    updated skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,55 +1,20 @@
 import React from "react";
 import '../App.css';
-import { FaBootstrap, FaFigma, FaGitAlt, FaGithub, FaHtml5, FaNodeJs, FaReact } from 'react-icons/fa';
-import { SiCsswizardry, SiCypress, SiExpress, SiHeroku, SiJavascript, SiJest, SiJira, SiNetlify, SiPostgresql, SiRedux, SiVercel, SiVisualstudiocode } from 'react-icons/si';
-import { TbApi } from 'react-icons/tb';
-import { AiOutlineConsoleSql } from "react-icons/ai";
-import { VscDebugAlt } from 'react-icons/vsc';
-import { GrDeploy } from 'react-icons/gr';
+import { skillData } from "./data/skillData";
+
 
 const Skills = () => {
   return (
     <section id="skills" className="skill-section">
       <h3>Skills</h3>
       <div className="skill-1">
-        <div className="skill-2">
-          <h4>Frontend</h4>
-          <p>
-            <FaReact />
-            <SiRedux />
-            <TbApi />
-            <SiJest />
-            <SiCypress />
-            <SiJavascript />
-            <FaHtml5 />
-            <SiCsswizardry />
-            <FaBootstrap />
-          </p>
-        </div>
-        <div className="skill-2">
-          <h4>Backend</h4>
-          <p>
-            <SiExpress />
-            <FaNodeJs />
-            <FaGitAlt />
-            <FaGithub />
-            <AiOutlineConsoleSql />
-            <SiPostgresql />
-            <SiVisualstudiocode />
-            <SiVercel />
-            <SiHeroku />
-            <SiNetlify />
-          </p>
-        </div>
-        <div className="skill-2">
-          <h4>Additional Skills</h4>
-          <p>
-            <VscDebugAlt />
-            <GrDeploy />
-            <SiJira />
-            <FaFigma />
-          </p>
-        </div>
+        {
+          skillData.map((skill => (
+            <div className="tooltip">{skill.icon}
+              <span className="tooltiptext">{skill.title}</span>
+            </div>
+          )))
+        }
       </div>
     </section >
   );
---
+++ src/components/data/skillData.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { FaBootstrap, FaFigma, FaGitAlt, FaGithub, FaHtml5, FaNodeJs, FaReact } from 'react-icons/fa';
+import { SiCsswizardry, SiCypress, SiExpress, SiHeroku, SiJavascript, SiJest, SiJira, SiNetlify, SiPostgresql, SiRedux, SiVercel, SiVisualstudiocode } from 'react-icons/si';
+import { TbApi } from 'react-icons/tb';
+import { AiOutlineConsoleSql } from "react-icons/ai";
+import { VscDebugAlt } from 'react-icons/vsc';
+import { GrDeploy } from 'react-icons/gr';
+
+
+export const skillData = [
+  {
+    title: 'React',
+    icon: <FaReact />
+  },
+  {
+    title: 'Redux',
+    icon: <SiRedux />
+  },
+  {
+    title: 'Api',
+    icon: <TbApi />
+  },
+  {
+    title: 'Jest',
+    icon: <SiJest />
+  },
+  {
+    title: 'Cypress',
+    icon: <SiCypress />
+  },
+  {
+    title: 'JavaScript',
+    icon: <SiJavascript />
+  },
+  {
+    title: 'HTML',
+    icon: <FaHtml5 />
+  },
+  {
+    title: 'CSS',
+    icon: <SiCsswizardry />
+  },
+  {
+    title: 'Bootstrap',
+    icon: <FaBootstrap />
+  },
+  {
+    title: 'Express',
+    icon: <SiExpress />
+  },
+  {
+    title: 'Node.js',
+    icon: <FaNodeJs />
+  },
+  {
+    title: 'Git',
+    icon: <FaGitAlt />
+  },
+  {
+    title: 'Github',
+    icon: <FaGithub />
+  },
+  {
+    title: 'SQL',
+    icon: <AiOutlineConsoleSql />
+  },
+  {
+    title: 'PostreSQL',
+    icon: <SiPostgresql />
+  },
+  {
+    title: 'VS Code',
+    icon: <SiVisualstudiocode />
+  },
+  {
+    title: 'Heroku',
+    icon: <SiHeroku />
+  },
+  {
+    title: 'Netlify',
+    icon: <SiNetlify />
+  },
+  {
+    title: 'Debugging',
+    icon: <VscDebugAlt />
+  },
+  {
+    title: 'Deployment',
+    icon: <GrDeploy />
+  },
+  {
+    title: 'Jira',
+    icon: <SiJira />
+  },
+  {
+    title: 'Figma',
+    icon: <FaFigma />
+  },
+
+];
